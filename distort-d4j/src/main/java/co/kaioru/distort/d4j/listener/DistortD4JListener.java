c9636da4821a0769a0d71fc775f52d79151687db
Keith <keifuurawrs@gmail.com>
2016-09-13 15:18:22 +0800

    Update Discord4J support for v2.6.0

--- distort-d4j/src/main/java/co/kaioru/distort/d4j/listener/DistortD4JListener.java
+++ distort-d4j/src/main/java/co/kaioru/distort/d4j/listener/DistortD4JListener.java
@@ -2,29 +2,29 @@ package co.kaioru.distort.d4j.listener;
 
 import co.kaioru.retort.CommandRegistry;
 import co.kaioru.retort.util.CommandUtil;
-import sx.blah.discord.api.IListener;
+import sx.blah.discord.api.events.IListener;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IMessage;
 
 public class DistortD4JListener implements IListener<MessageReceivedEvent> {
 
-	private final CommandRegistry registry;
+    private final CommandRegistry registry;
 
-	public DistortD4JListener(CommandRegistry registry) {
-		this.registry = registry;
-	}
+    public DistortD4JListener(CommandRegistry registry) {
+        this.registry = registry;
+    }
 
-	@Override
-	public void handle(MessageReceivedEvent event) {
-		try {
-			IMessage message = event.getMessage();
-			String content = message.getContent();
+    @Override
+    public void handle(MessageReceivedEvent event) {
+        try {
+            IMessage message = event.getMessage();
+            String content = message.getContent();
 
-			if (content.startsWith("."))
-				CommandUtil.executeCommand(registry, CommandUtil.getArgsFromText(content.substring(1, content.length())), message);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            if (content.startsWith("."))
+                CommandUtil.executeCommand(registry, CommandUtil.getArgsFromText(content.substring(1, content.length())), message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
