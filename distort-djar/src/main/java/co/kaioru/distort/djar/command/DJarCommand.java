9000f4b6d65af5ebfedb3260d52fecded91bd0ab
Kaioru <keifuurawrs@gmail.com>
2017-04-01 11:18:28 +0800

    Add preliminary Discord.jar support

---
+++ distort-djar/src/main/java/co/kaioru/distort/djar/command/DJarCommand.java
@@ -0,0 +1,9 @@
+package co.kaioru.distort.djar.command;
+
+import co.kaioru.retort.command.Command;
+
+public abstract class DJarCommand extends Command implements DJarCommandExecutable {
+
+	//
+
+}
---
+++ distort-djar/src/main/java/co/kaioru/distort/djar/command/DJarCommandBuilder.java
@@ -0,0 +1,36 @@
+package co.kaioru.distort.djar.command;
+
+import co.kaioru.retort.util.builder.CommandBuilder;
+import discord.jar.UserChatEvent;
+
+import java.util.LinkedList;
+
+public class DJarCommandBuilder extends CommandBuilder<DJarCommandBuilder, DJarCommand, DJarCommandExecutable> {
+
+	public DJarCommandBuilder(String name) {
+		super(name);
+	}
+
+	@Override
+	public DJarCommand build(DJarCommandExecutable dJarCommandExecutable) {
+		return new DJarCommand() {
+
+			@Override
+			public String getName() {
+				return getName();
+			}
+
+			@Override
+			public String getDesc() {
+				return getDesc();
+			}
+
+			@Override
+			public void execute(LinkedList<String> args, UserChatEvent event) throws Exception {
+				dJarCommandExecutable.execute(args, event);
+			}
+
+		};
+	}
+
+}
