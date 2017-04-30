7eaf4cc3e97cfc62469b4b532b6aa6c5e90abb6b
Kaioru <keifuurawrs@gmail.com>
2017-04-30 10:03:56 +0800

    Remove everything!

--- distort-djar/src/main/java/co/kaioru/distort/djar/command/DJarCommand.java
+++
@@ -1,9 +0,0 @@
-package co.kaioru.distort.djar.command;
-
-import co.kaioru.retort.command.Command;
-
-public abstract class DJarCommand extends Command implements DJarCommandExecutable {
-
-	//
-
-}
--- distort-jc/src/main/java/co/kaioru/distort/jc/command/JCCommand.java
+++
@@ -1,9 +0,0 @@
-package co.kaioru.distort.jc.command;
-
-import co.kaioru.retort.command.Command;
-
-public abstract class JCCommand extends Command implements JCCommandExecutable {
-
-	//
-
-}
