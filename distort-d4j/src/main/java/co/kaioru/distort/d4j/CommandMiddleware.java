c897c395a4fe76bcfb4fc5b22f9d3360ab8ec741
Kaioru <keifuurawrs@gmail.com>
2017-05-03 14:47:09 +0800

    Delete everything again

--- distort-d4j/src/main/java/co/kaioru/distort/d4j/CommandMiddleware.java
+++
@@ -1,9 +0,0 @@
-package co.kaioru.distort.d4j;
-
-import co.kaioru.retort.AbstractCommandMiddleware;
-
-public abstract class CommandMiddleware extends AbstractCommandMiddleware<CommandContext> {
-
-    //
-
-}
