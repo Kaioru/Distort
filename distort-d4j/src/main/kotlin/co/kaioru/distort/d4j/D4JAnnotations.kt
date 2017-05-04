6968bc9f7feec937b67cc60f5111c31c348f8558
Kaioru <keifuurawrs@gmail.com>
2017-05-04 08:49:54 +0800

    Auto register adapters and providers

--- distort-d4j/src/main/kotlin/co/kaioru/distort/d4j/D4JAnnotations.kt
+++ distort-d4j/src/main/kotlin/co/kaioru/distort/d4j/D4JAnnotations.kt
@@ -3,8 +3,15 @@ package co.kaioru.distort.d4j
 import co.kaioru.avoid.annotation.SimpleAnnotationFactory
 import co.kaioru.avoid.annotation.SimpleAnnotationGenerator
 import co.kaioru.avoid.annotation.SimpleAnnotationProcessor
+import co.kaioru.retort.annotation.impl.adapter.basicGenerators
+import co.kaioru.retort.annotation.impl.adapter.basicProcessors
 
-class D4JAnnotationFactory : SimpleAnnotationFactory<D4JContext>()
+class D4JAnnotationFactory : SimpleAnnotationFactory<D4JContext>() {
+    init {
+        registerAdapters(basicGenerators(D4JContext::class.java, Void::class.java))
+        registerAdapters(basicProcessors())
+    }
+}
 
 abstract class D4JAnnotationGenerator : SimpleAnnotationGenerator<D4JContext>()
 
--- distort-d4j/src/main/kotlin/co/kaioru/distort/d4j/D4JReflections.kt
+++ distort-d4j/src/main/kotlin/co/kaioru/distort/d4j/D4JReflections.kt
@@ -2,7 +2,12 @@ package co.kaioru.distort.d4j
 
 import co.kaioru.avoid.reflection.SimpleReflectionBuilder
 import co.kaioru.avoid.reflection.SimpleReflectionProvider
+import co.kaioru.retort.reflection.impl.provider.primitiveProviders
 
-class D4JReflectionGenerator : SimpleReflectionBuilder<D4JContext>()
+class D4JReflectionGenerator : SimpleReflectionBuilder<D4JContext>() {
+    init {
+        registerProviders(primitiveProviders())
+    }
+}
 
 abstract class D4JReflectionProvider<O : Any> : SimpleReflectionProvider<D4JContext, O>()
